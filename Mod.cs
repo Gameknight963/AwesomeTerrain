b16495c745c0891c0f3b78cfac4a2448303403ae
sebi <sebisebpaw@gmail.com>
2026-05-03 14:38:29 -0500

    multiplayer support + bugfixes
     - fixed looking in the wrong place for grass texture
     - made WaterVolume a component of the Water gameobject instead of the
       Camera
     - added multiplayer via Multiside

--- Mod.cs
+++ Mod.cs
@@ -7,24 +7,40 @@ namespace AwesomeTerrain
     public class Mod : MelonMod
     {
         public static GameObject Kiri;
+        private static TerrainGenerator generator;
+        
+        public static int Seed { get; private set; }
+
+        public static void SetSeedAndGenerate(int newSeed)
+        {
+            Seed = newSeed;
+            generator?.Generate(newSeed);
+        }
 
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
-            if (sceneName != "Version 1.9 POST") return;
+            if (sceneName != "Version 1.9 POST")
+            {
+                generator = null;
+                Seed = 0;
+                return;
+            }
 
             GameObject terrainGen = new GameObject("TerrainGenerator");
-            terrainGen.AddComponent<TerrainGenerator>();
-            GameObject.Find("World/House/Doors/ExitFrame/ExitDoor").SetActive(false);
-            GameObject.Find("World/Game/Acts/Hello Mita/Interactables 1/I Exit Door 1 ").SetActive(false);
-            GameObject.Find("World/Game/Acts/Quality Time/Interactables 2/I ExitDoor 2").SetActive(false);
+            generator = terrainGen.AddComponent<TerrainGenerator>();
+            
+            if (!Networking.Available)
+                SetSeedAndGenerate(Random.Range(int.MinValue, int.MaxValue));
+
+            GameObject.Find("World/House/Doors/ExitFrame/ExitDoor")?.SetActive(false);
+            GameObject.Find("World/Game/Acts/Hello Mita/Interactables 1/I Exit Door 1 ")?.SetActive(false);
+            GameObject.Find("World/Game/Acts/Quality Time/Interactables 2/I ExitDoor 2")?.SetActive(false);
 
             OOBProtection oob = GameObject.FindObjectOfType<OOBProtection>();
             oob.limit = -300;
 
             GameObject playerCamera = GameObject.Find("playerCamera");
             playerCamera.GetComponent<WorldOutline>().enabled = false;
-
-            playerCamera.AddComponent<WaterVolume>();
 
             RenderSettings.fogColor = new Color(1f, 1f, 1f, 1f);
             RenderSettings.fogDensity = 0.01f;
---
+++ Networking.cs
@@ -0,0 +1,67 @@
+﻿using MultiSide.shared;
+using Photon.Client;
+using System;
+using System.Linq;
+using UnityEngine.Profiling;
+
+namespace AwesomeTerrain
+{
+    public static class Networking
+    {
+        const string ChannelSeedRequest = "awesometerrain.seed.request";
+        const string ChannelSeedResponse = "awesometerrain.seed.response";
+        public static bool Available { get; set; }
+
+        static Networking()
+        {
+            Available = AppDomain.CurrentDomain.GetAssemblies()
+            .Any(a => a.GetName().Name == "Multiside.shared");
+            if (Available) InitNetwork();
+        }
+
+        private static void InitNetwork()
+        {
+            if (NetworkRegistry.Provider != null)
+            {
+                Subscribe(NetworkRegistry.Provider);
+                return;
+            }
+            NetworkRegistry.OnProviderRegistered += Subscribe;
+        }
+
+        private static void Subscribe(INetworkProvider provider)
+        {
+            provider.OnRoomJoined += Provider_OnRoomJoined;
+            provider.OnReceived += Provider_OnReceived;
+        }
+
+        private static void Provider_OnRoomJoined()
+        {
+            if (!(NetworkRegistry.Provider is INetworkProvider provider)) return;
+
+            if (provider.IsMasterClient)
+            {
+                Mod.SetSeedAndGenerate(UnityEngine.Random.Range(int.MinValue, int.MaxValue));
+                return;
+            }
+            provider.Send(ChannelSeedRequest, new PhotonHashtable());
+        }
+
+        private static void Provider_OnReceived(int actor, string channel, object data)
+        {
+            if (!(NetworkRegistry.Provider is INetworkProvider provider)) return;
+
+            switch (channel)
+            {
+                case ChannelSeedRequest:
+                    if (!provider.IsMasterClient) return;
+                    provider.SendTo(actor, ChannelSeedResponse, Mod.Seed);
+                    break;
+                case ChannelSeedResponse:
+                    if (provider.MasterClientActorNumber != actor) return;
+                    Mod.SetSeedAndGenerate((int)data);
+                    break;
+            }
+        }
+    }
+}
